786c82c95cf810d30b00865536a0a681b47ebdf2
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-05-08 11:42:36 -0400

    Create RankUp.php

---
+++ src/esh123cookie/rankup/RankUp.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace esh123cookie\rankup;
+
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\command\CommandExecutor;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\Player;
+use pocketmine\level\sound\PopSound;
+use pocketmine\event\Listeners;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\Task;
+use pocketmine\level\Level;
+use pocketmine\Server;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\network\mcpe\protocol\ChangeDimensionPacket;
+use pocketmine\network\mcpe\protocol\types\DimensionIds;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\network\mcpe\protocol\PlayerListPacket;
+use pocketmine\network\mcpe\protocol\types\PlayerListEntry;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\tile\EnderChest;
+use pocketmine\tile\Tile;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerBedEnterEvent;
+
+class RankUp extends PluginBase{
+
+
+
+
+
