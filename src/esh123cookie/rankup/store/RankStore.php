037aa725d4736d5f469b1613e49d16f1892ce330
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-10-05 13:50:41 -0400

    Update RankStore.php

--- src/esh123cookie/rankup/store/RankStore.php
+++ src/esh123cookie/rankup/store/RankStore.php
@@ -11,6 +11,10 @@ class RankStore {
     private $config;
 	
     private $plugin;
+	
+    public $price;
+
+    public $rank;
 
     public function __construct(RankUp $plugin) {
         $this->plugin = $plugin;
@@ -19,3 +23,23 @@ class RankStore {
     public function getPlugin(){
 	return $this->plugin;
     }
+	
+    public function setRankConfig(): void { 
+	    foreach($this->getRanks() as $this->rank) { 
+	    foreach($this->getRankPrices() as $this->price) {
+	    }
+	}
+    }    
+	
+    public function getRankPrices(): array { 
+      	    $prices = new Config($this->getDataFolder() . "/ranks.yml", Config::YAML);
+            $rankPrices = $prices->getAll()["price"];
+            return $rankPrices;
+    }
+	
+    public function getRanks(): array { 
+      	    $ranks = new Config($this->getDataFolder() . "/ranks.yml", Config::YAML);
+            $rank = $ranks->getAll()["rank"];
+            return $rank;
+    }
+}
