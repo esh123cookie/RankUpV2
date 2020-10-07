e348ff33718475da2986014db1bbf04cc72637ee
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-10-06 22:33:22 -0400

    Create RankUpCommand.php

---
+++ src/esh123cookie/rankup/command/RankUpCommand.php
@@ -0,0 +1,20 @@
+      /*
+      public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+      {
+      	   $rank = $this->getRank($sender, $this->getConfig()->get("rank"));
+	   if($cmd->getName() == "ruabout") {
+	      if ($sender instanceof Player) {
+	      $sender->sendMessage("§7(§a!§7) Plugin made by: esh123cookie for custom plugins message me on my discord @bigbozzlmao#4035"); 
+              }
+              return true;
+	   }
+	   if($cmd->getName() == "mines") {
+	      if ($sender instanceof Player) {
+		  $this->mines($sender);
+              }
+              return true;
+	   }
+           if($cmd->getName() == "rankup") {
+	   }
+      }
+      */
