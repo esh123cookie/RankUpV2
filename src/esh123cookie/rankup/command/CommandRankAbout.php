2c9763577898b27fe37acdfc2f9dab74422738d1
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-10-09 20:17:30 -0400

    Create CommandRankAbout.php

---
+++ src/esh123cookie/rankup/command/CommandRankAbout.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace esh123cookie\rankup\command;
+
+use esh123cookie\rankup\RankUp;
+use esh123cookie\rankup\store\RankStore; 
+use esh123cookie\rankup\command\CommandStore; 
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\level\sound\PopSound;
+
+class CommandRankAbout extends PluginCommand{
+
+    private $owner;
+
+    public function __construct(string $name, RankUp $owner)
+    {
+        parent::__construct($name, $owner);
+        $this->owner = $owner;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player) { 
+           $data = new CommandStore($this->getPlugin());
+           $data->about($sender);
+        }
+    }
+}
